a3828574d2d7867ffaaa368f491250b7fe8cee90
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-20 19:08:59 -0600

    Finished the next knowledge check

--- fundamentals.py
+++ fundamentals.py
@@ -1,6 +1,20 @@
-x = 10
-name = gabriel
-is_true = True
+x = -10
+name = 'gabriel'
+is_true = False
 print(x)
 print(name)
 print(is_true)
+
+if(x > 10):
+    print('That is larger than 10')
+else:
+    print('That is not larger than 10')
+
+if(x < 0 and is_true == True):
+    print('Negative and True')
+elif(x > 0 and is_true == False):
+    print('Positive and False')
+elif(x > 100 or is_true == True):
+    print('Large or True')
+else:
+    print('I dont know')
